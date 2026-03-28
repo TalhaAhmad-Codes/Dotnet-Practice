7b38c546d1d37aceb45a546fb07ebf8844fa37ff
Talha Ahmad <talha.ahmad.tech@gmail.com>
2026-03-28 20:25:32 +0500

    Create my first ML model using ML.NET

---
+++ ML.NET/Practice/AIApp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace AIApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Load sample data
+            var sampleData = new ClassifierModel.ModelInput()
+            {
+                Col0 = @"I love you.",
+            };
+
+            // Load model and predict output
+            var result = ClassifierModel.Predict(sampleData);
+
+            // Display the result
+            Console.WriteLine($"Text:\t\t{sampleData.Col0}");
+            var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+            Console.WriteLine($"Sentiment:\t{sentiment}");
+            Console.WriteLine($"\nPossitive:\t{result.Score[0] * 100}%");
+            Console.WriteLine($"Negative:\t{result.Score[1] * 100}%");
+        }
+    }
+}
