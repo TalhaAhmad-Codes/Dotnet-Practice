1dd26dbbf06f8119af071179f977fba8d4886979
Talha Ahmad <talha.ahmad.tech@gmail.com>
2026-03-31 15:16:59 +0500

    Fix bugs and tested...

--- AI/LLMs/Practice/OllamaLLM/Program.cs
+++ AI/LLMs/Practice/OllamaLLM/Program.cs
@@ -7,6 +7,7 @@ using OllamaSharp;
 var builder = Host.CreateApplicationBuilder(args);
 
 // Register Ollama client
+Console.WriteLine("\nConnecting...");
 builder.Services.AddSingleton(new OllamaApiClient(new Uri("http://localhost:11434"))
 {
     SelectedModel = "llama3"
@@ -15,6 +16,7 @@ builder.Services.AddSingleton(new OllamaApiClient(new Uri("http://localhost:1143
 // Register your service
 // 1. Install Ollama on your system: irm https://ollama.com/install.ps1 | iex
 // 2. Run server: ollama serve
+Console.WriteLine("\nBuilding...");
 builder.Services.AddSingleton<IChatService, ChatService>();
 
 var app = builder.Build();
@@ -22,5 +24,16 @@ var app = builder.Build();
 // Resolve and run
 var chatService = app.Services.GetRequiredService<IChatService>();
 
-var result = await chatService.AskAsync("What is Ollama?");
-Console.WriteLine(result);
+Console.Write("\nPrompt: ");
+var prompt = Console.ReadLine();
+
+if (prompt != null)
+{
+    Console.WriteLine("\nGetting Response...");
+    var result = await chatService.AskAsync(prompt);
+
+    Console.WriteLine("\nThe Response:");
+    Console.WriteLine(result);
+}
+
+Console.WriteLine("\nGoody Bye... ;-)");
