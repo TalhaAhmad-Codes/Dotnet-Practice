286e72822929c7dbde04d494f22e3a4dd294277b
Talha Ahmad <talha.ahmad.tech@gmail.com>
2026-04-25 19:31:10 +0500

    Covered Parent-Child communication using parameterized components in Blazor.

---
+++ Blazor/MyApp/Modals/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace MyApp.Modals
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<string> Hobbies { get; set; }
+    }
+}
